4bf32121af0231dc77e95ef02b15f791c714b56a
lucky-smash <tyagilakshit6@gmail.com>
2025-07-23 17:54:19 +0530

    Add files via upload

---
+++ JavaScript Variables, Data Types & Objects | Sigma Web Development Course - Tutorial #55/script.js
@@ -0,0 +1,56 @@
+// console.log("hey this is tutorial 55 of javascript")
+
+// var a = 5;
+// a = a + 1;
+// var b = 10;
+// var c = "lucky";
+// console.log(a+b);
+// console.log(typeof a ,typeof b ,typeof c);
+
+// const a1 = 6;
+// // a1 = a1 + 1; not possible  
+
+// ==================================================================================================
+
+
+// we always use let instead  of var 
+
+//  let a = 5;
+
+//  {
+//     let a = 10;
+//     console.log(a);
+//  }
+// console.log(a)
+
+//  var a = 5; 
+
+//  {
+//     var a = 10;
+//     console.log(a);
+//  }
+// console.log(a)
+
+// ======================================================================================================
+// let x = "Harry bhai";
+// let y = 22;
+// let z = 3.55;
+// const p = true;
+// let q = undefined;
+// let r = null;
+
+// console.log(x, y, z, p, q, r)
+// console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+// ====================================================================================================
+
+let o ={
+    "name": "lakshit",
+    "job code": 5600,
+    "is_handsome" : true 
+}
+console.log(o);
+o.salary = "100 crores";
+console.log(o); 
+o.salary = "500 crores";
+console.log(o); 
